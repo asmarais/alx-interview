8df75a64e0a198e09ccfa66d35892feeacfe8c73
asmarais <rais.asma99@gmail.com>
2024-09-30 13:04:50 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+0-lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+     Returns True if all boxes can be opened,
+     else return False
+    """
+    n = len(boxes)
+    opened_boxes = set()
+    opened_boxes.add(0)
+    stack = [0]
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key < n and key not in opened_boxes:
+                opened_boxes.add(key)
+                stack.append(key)
+    print(opened_boxes)
+    return len(opened_boxes) == n
